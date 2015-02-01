2878a0504f0c8643daac5fc01df491ac090aa98f
mohammed <mohammed@elgadimo>
2015-02-01 01:46:48 +0100

    adding search form

--- gen/com/sopra/passport/R.java
+++ gen/com/sopra/passport/R.java
@@ -27,6 +27,7 @@ public final class R {
         public static final int gradient_bg_hover=0x7f020002;
         public static final int ic_launcher=0x7f020003;
         public static final int list_selector=0x7f020004;
+        public static final int logo=0x7f020005;
     }
     public static final class id {
         public static final int action_settings=0x7f080040;
